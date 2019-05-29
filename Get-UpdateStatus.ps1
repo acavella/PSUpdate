94f9d05b4d71d2c40dc803cb2698eb7cf12b6e51
Tony Cavella <tony@cavella.com>
2019-05-29 19:23:58 -0400

    Added process section

--- Get-UpdateStatus.ps1
+++ Get-UpdateStatus.ps1
@@ -37,13 +37,15 @@
     #>
     Param
     (
-        [parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [String]$Path,
-		[String]$Name
+		[Switch]$Debug,
+		[String[]]$ComputerName	
     )
-	Begin 
-	{
+	Begin {
+		If($PSBoundParameters['Debug'])
+		{
+			$DebugPreference = "Continue"
+		}
+
     	$User = [Security.Principal.WindowsIdentity]::GetCurrent()
 		$Role = (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 	
@@ -52,6 +54,91 @@
 		Write-Warning "This function must be run with Administrative privileges."	
 		} 
     }
-    Process {}
+    Process {
+		
+		Write-Debug "Check if ComputerName is set"
+		If($ComputerName -eq $null)
+		{
+			Write-Debug "Set ComputerName to $env:COMPUTERNAME"
+			[String[]]$ComputerName = $env:COMPUTERNAME
+		} 
+
+				$UpdateCollection = @()
+		Foreach($Computer in $ComputerName)
+		{
+			If(Test-Connection -ComputerName $Computer -Quiet)
+			{
+				#region STAGE 1
+				Write-Debug "STAGE 1: Get history list"
+				###################################
+				# Start STAGE 1: Get history list #
+				###################################
+		
+				#If ($pscmdlet.ShouldProcess($Computer,"Get updates history")) 
+				#{
+					Write-Verbose "Get updates history for $Computer"
+					If($Computer -eq $env:COMPUTERNAME)
+					{
+						Write-Debug "Create Microsoft.Update.Session object for $Computer"
+						$objSession = New-Object -ComObject "Microsoft.Update.Session" #Support local instance only
+					} #End If $Computer -eq $env:COMPUTERNAME
+					Else
+					{
+						Write-Debug "Create Microsoft.Update.Session object for $Computer"
+						$objSession =  [activator]::CreateInstance([type]::GetTypeFromProgID("Microsoft.Update.Session",$Computer))
+					} #End Else $Computer -eq $env:COMPUTERNAME
+
+					Write-Debug "Create Microsoft.Update.Session.Searcher object for $Computer"
+					$objSearcher = $objSession.CreateUpdateSearcher()
+					$TotalHistoryCount = $objSearcher.GetTotalHistoryCount()
+
+					If($TotalHistoryCount -gt 0)
+					{
+						$objHistory = $objSearcher.QueryHistory(0, $TotalHistoryCount)
+						$NumberOfUpdate = 1
+						Foreach($obj in $objHistory)
+						{
+							Write-Progress -Activity "Get update histry for $Computer" -Status "[$NumberOfUpdate/$TotalHistoryCount] $($obj.Title)" -PercentComplete ([int]($NumberOfUpdate/$TotalHistoryCount * 100))
+
+							Write-Debug "Get update histry: $($obj.Title)"
+							Write-Debug "Convert KBArticleIDs"
+							$matches = $null
+							$obj.Title -match "KB(\d+)" | Out-Null
+							
+							If($matches -eq $null)
+							{
+								Add-Member -InputObject $obj -MemberType NoteProperty -Name KB -Value ""
+							} #End If $matches -eq $null
+							Else
+							{							
+								Add-Member -InputObject $obj -MemberType NoteProperty -Name KB -Value ($matches[0])
+							} #End Else $matches -eq $null
+							
+							Add-Member -InputObject $obj -MemberType NoteProperty -Name ComputerName -Value $Computer
+							
+							$obj.PSTypeNames.Clear()
+							$obj.PSTypeNames.Add('PSWindowsUpdate.WUHistory')
+						
+							$UpdateCollection += $obj
+							$NumberOfUpdate++
+						} #End Foreach $obj in $objHistory
+						Write-Progress -Activity "Get update histry for $Computer" -Status "Completed" -Completed
+					} #End If $TotalHistoryCount -gt 0
+					Else
+					{
+						Write-Warning "Probably your history was cleared. Alternative please run 'Get-WUList -IsInstalled'"
+					} #End Else $TotalHistoryCount -gt 0
+				#} #End If $pscmdlet.ShouldProcess($Computer,"Get updates history")
+				
+				################################
+				# End PASS 1: Get history list #
+				################################
+				#endregion
+				
+			} #End If Test-Connection -ComputerName $Computer -Quiet
+		} #End Foreach $Computer in $ComputerName	
+		
+		Return $UpdateCollection
+	}
     End {}
 #}
